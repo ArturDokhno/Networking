e678954fedd4d16752c05be92facaa9526f9d254
ArturDokhno <artur.dohno@icloud.com>
2022-04-17 11:31:01 +0500

    Загрузка картинки из сети, с индикатором загрузки

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
     }
-
-
+    
+    @IBAction func getImage(_ sender: Any) {
+        
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg") else { return }
+        
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
+    }
 }
 
