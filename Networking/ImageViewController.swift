bb54e2e33ead18c4e027e2118e858f3ce74754a5
ArturDokhno <artur.dohno@icloud.com>
2022-07-27 14:54:16 +0500

    Добавление главного экрана с тремя кнопками(Download, GET, POST)

---
+++ Networking/ImageViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ImageViewController.swift
+//  Networking
+//
+//  Created by Артур Дохно on 27.07.2022.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
+        fetchImage()
+    }
+    
+    func fetchImage() {
+        
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg") else { return }
+        
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
+    }
+    
+}
+
--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -9,33 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var activityIndicator: UIActivityIndicatorView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func getRequest(_ sender: UIButton) {
         
-        activityIndicator.isHidden = true
-        activityIndicator.hidesWhenStopped = true
     }
     
-    @IBAction func getImage(_ sender: Any) {
+    @IBAction func postRequest(_ sender: Any) {
         
-        activityIndicator.isHidden = false
-        activityIndicator.startAnimating()
-        
-        guard let url = URL(string: "https://rickandmortyapi.com/api/character/avatar/1.jpeg") else { return }
-        
-        let session = URLSession.shared
-        
-        session.dataTask(with: url) { (data, response, error) in
-            if let data = data, let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self.activityIndicator.stopAnimating()
-                    self.imageView.image = image
-                }
-            }
-        }.resume()
     }
+    
 }
-
