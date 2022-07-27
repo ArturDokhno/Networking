dae16dcc3b6a2f093780a5880f265c85339ef636
ArturDokhno <artur.dohno@icloud.com>
2022-07-27 15:16:10 +0500

    GET запрос

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBAction func getRequest(_ sender: UIButton) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            guard let response = response, let data = data else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+        }.resume()
     }
     
     @IBAction func postRequest(_ sender: Any) {
